a4212d5b265c53cc0ab31bb25ef5ba6cc9a4ca97
C1296849 Ramon Cruz da Silva <c1296849@interno.bb.com.br>
2020-06-25 09:24:40 -0300

    Testes com typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Service from './services/api';
+import { User } from './components';
+interface IUser {
+  name: string;
+  email: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(()=>{
+    Service.get<IUser[]>('/users').then(response=>{
+      setUsers(response.data);
+    })
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {users.map(user => <User user={user}/>)}
     </div>
   );
 }
